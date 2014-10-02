bfb8db8e100fcdebc81e3f62187bde16c77afe59
jrogozen <jon.rogozen@gmail.com>
2014-10-02 22:36:30 +0000

    change class to model.self

--- lib/rock_pape_scis.rb
+++ lib/rock_pape_scis.rb
@@ -1,50 +1,47 @@
 require "rock_pape_scis/version"
 
 module RockPapeScis
-  # Your code goes here...
-  class RPS
-    attr_reader :player_1, :player_2
+  
+  def self.play(opts)
+    player_1 = opts[:player_1][:username]
+    player_2 = opts[:player_2][:username]
 
-    def play(opts)
-      @player_1 = opts[:player_1][:name]
-      @player_2 = opts[:player_2][:name]
-
-      fight(opts[:player_1], opts[:player_2])
-    end
-
-    def fight(player_1, player_2)
-      # return dat tie
-      move1 = player_1[:move]
-      move2 = player_2[:move]
-
-      if move1 == move2
-        return "tie"
-      end
-
-      if move1 == "rock"
-        if move2 == "scissor"
-          return player_1[:name]
-        else
-          return player_2[:name]
-        end
-      end
-
-      if move1 == "paper"
-        if move2 == "rock"
-          return player_1[:name]
-        else
-          return player_2[:name]
-        end
-      end
-
-      if move1 == "scissor"
-        if move2 == "paper"
-          return player_1[:name]
-        else
-          return player_2[:name]
-        end
-      end
-    end
-
+    fight(opts[:player_1], opts[:player_2])
   end
+
+  def self.fight(player_1, player_2)
+    # return dat tie
+    move1 = player_1[:move]
+    move2 = player_2[:move]
+
+    if move1 == move2
+      return "tie"
+    end
+
+    if move1 == "rock"
+      if move2 == "scissor"
+        return player_1[:username]
+      else
+        return player_2[:username]
+      end
+    end
+
+    if move1 == "paper"
+      if move2 == "rock"
+        return player_1[:username]
+      else
+        return player_2[:username]
+      end
+    end
+
+    if move1 == "scissor"
+      if move2 == "paper"
+        return player_1[:username]
+      else
+        return player_2[:username]
+      end
+    end
+  end
+
+
 end
--- spec/rock_pape_scis_spec.rb
+++ spec/rock_pape_scis_spec.rb
@@ -1,54 +1,39 @@
 require_relative 'spec_helper.rb'
 
 describe RockPapeScis do
-  describe RockPapeScis::RPS do
-    let(:rps) {RockPapeScis::RPS.new}
-    describe '#play' do 
-      it 'assigns players to variables' do 
-        rps = RockPapeScis::RPS.new
-        rps.play({
-          player_1: {name: "Jon", move: "paper"}, 
-          player_2: {name: "DJ", move: "rock"}
-        })
-        expect(rps.player_1).to eq("Jon")
-        expect(rps.player_2).to eq("DJ")
-      end
+  describe '#fight' do
+    it 'determines a tie' do 
+      result = RockPapeScis.play({
+        player_1: {username: "Jon", move: "rock"}, 
+        player_2: {username: "DJ", move: "rock"}
+      })
+      expect(result).to eq("tie")
     end
 
-    describe '#fight' do
-      it 'determines a tie' do 
-        result = rps.play({
-          player_1: {name: "Jon", move: "rock"}, 
-          player_2: {name: "DJ", move: "rock"}
-        })
-        expect(result).to eq("tie")
-      end
-
-      it 'determines paper beats rock' do
-        result = rps.play({
-          player_1: {name: "Jon", move: "rock"}, 
-          player_2: {name: "DJ", move: "paper"}
-        }) 
-        expect(result).to eq("DJ")
-      end
-
-       it 'determines rock beats scissor' do
-        result = rps.play({
-          player_1: {name: "Jon", move: "rock"}, 
-          player_2: {name: "DJ", move: "scissor"}
-        }) 
-        expect(result).to eq("Jon")
-      end
-
-       it 'determines scissor beats paper' do
-        result = rps.play({
-          player_1: {name: "Jon", move: "paper"}, 
-          player_2: {name: "DJ", move: "scissor"}
-        }) 
-        expect(result).to eq("DJ")
-      end
-
+    it 'determines paper beats rock' do
+      result = RockPapeScis.play({
+        player_1: {username: "Jon", move: "rock"}, 
+        player_2: {username: "DJ", move: "paper"}
+      }) 
+      expect(result).to eq("DJ")
     end
+
+     it 'determines rock beats scissor' do
+      result = RockPapeScis.play({
+        player_1: {username: "Jon", move: "rock"}, 
+        player_2: {username: "DJ", move: "scissor"}
+      }) 
+      expect(result).to eq("Jon")
+    end
+
+     it 'determines scissor beats paper' do
+      result = RockPapeScis.play({
+        player_1: {username: "Jon", move: "paper"}, 
+        player_2: {username: "DJ", move: "scissor"}
+      }) 
+      expect(result).to eq("DJ")
+    end
+
   end
 end
 
